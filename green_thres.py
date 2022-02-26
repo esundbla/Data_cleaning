a86fbb72a124254f4eefe98651e8cae3ed7265e2
esundbla <69823637+esundbla@users.noreply.github.com>
2022-02-26 00:52:40 -0700

    Plant_lab Image Processing

--- green_thres.py
+++ green_thres.py
@@ -4,18 +4,22 @@ import numpy as np
 
 
 if __name__=="__main__":
-    src = cv.imread('S008/06-12-2019/325803.JPG')
+    src = cv.imread('S008/06-05-2019/104403.JPG')
     rgb = cv.cvtColor(src, cv.COLOR_BGR2RGB)
     plt.figure()
     plt.title('original')
     plt.imshow(rgb)
     mod = rgb
     length, width, extra = rgb.shape
+    grn_pix_count = 0
     for i in range(length):
         for j in range(width):
             if rgb[i,j,2] > 145:
                 mod[i, j, :] = 0
+            if mod[i, j, 0] > 100 and mod[i, j, 1]  > 100 and mod[i, j, 2] > 70:
+                grn_pix_count += 1
     plt.figure()
     plt.title("mod")
     plt.imshow(mod)
     plt.show()
+    print(grn_pix_count)
