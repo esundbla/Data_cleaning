10a738c4320fe7dfd839f7d24636026fe0568111
esundbla <69823637+esundbla@users.noreply.github.com>
2022-03-02 01:28:24 -0700

    plant lab initial work

---
+++ testing.py
@@ -0,0 +1,13 @@
+import cv2 as cv
+
+if __name__ == "__main__":
+    while(True):
+        try:
+            img = cv.imread("img.jpg")
+            rgb = cv.
+        except (cv.error, OpenCV Error):
+            print
+            "Can't do the thing"
+            continue
+    print("still going")
+    exit(0)
